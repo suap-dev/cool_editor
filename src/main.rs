b026d8667bfa59046e49d29546b2889fb247cc6c
suap-dev <suap.dev@gmail.com>
2024-09-09 23:23:52 +0200

    little refactor for readibility

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::io;
 use std::path::Path;
 use std::sync::Arc;
 
+use iced::executor;
 use iced::widget::{column, container, horizontal_space, row, text, text_editor};
-use iced::{executor, Command};
-use iced::{Application, Element, Length, Settings, Theme};
+use iced::{Application, Command, Element, Length, Settings, Theme};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -16,25 +16,25 @@ struct Editor {
     content: text_editor::Content,
 }
 #[derive(Debug, Clone)]
-enum EditorMessage {
+enum Message {
     Edit(text_editor::Action),
     FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
 impl Application for Editor {
-    type Message = EditorMessage;
+    type Message = Message;
     type Theme = Theme;
     type Executor = executor::Default;
     type Flags = ();
 
-    fn new(_flags: Self::Flags) -> (Editor, iced::Command<Self::Message>) {
+    fn new(_flags: ()) -> (Editor, iced::Command<Message>) {
         (
             Self {
                 content: text_editor::Content::new(),
             },
             Command::perform(
                 load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
-                Self::Message::FileOpened,
+                Message::FileOpened,
             ),
         )
     }
@@ -43,34 +43,36 @@ impl Application for Editor {
         String::from("A cool editor!")
     }
 
-    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Self::Message::Edit(action) => self.content.edit(action),
-            Self::Message::FileOpened(result) => {
+            Message::Edit(action) => self.content.edit(action),
+            Message::FileOpened(result) => {
                 if let Ok(file_content) = result {
                     self.content = text_editor::Content::with(&file_content);
                 }
             }
         }
+
         Command::none()
     }
 
-    fn view(&self) -> Element<'_, Self::Message> {
-        let text_input = text_editor(&self.content).on_edit(Self::Message::Edit);
-        let cursor_position = {
-            let (line, column) = self.content.cursor_position();
-            text(format!("L:{}, C:{}", line + 1, column + 1))
+    fn view(&self) -> Element<'_, Message> {
+        let content = {
+            let text_editor = text_editor(&self.content).on_edit(Message::Edit);
+            let status_bar = {
+                let cursor_status = {
+                    let (line, column) = self.content.cursor_position();
+                    text(format!("L:{}, C:{}", line + 1, column + 1))
+                };
+                row![horizontal_space(Length::Fill), cursor_status]
+            };
+            column![text_editor, status_bar].spacing(5)
         };
-
-        let status_bar = row![horizontal_space(Length::Fill), cursor_position];
-
-        container(column![text_input, status_bar].spacing(5))
-            .padding(10)
-            .into()
+        container(content).padding(10).into()
     }
 
-    fn theme(&self) -> iced::Theme {
-        iced::Theme::Dark
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
 

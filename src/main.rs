ffac3e79a35f0e60b64603961ef1776719f7d983
suap-dev <suap.dev@gmail.com>
2024-09-10 01:43:03 +0200

    added open_file button

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ use std::path::Path;
 use std::sync::Arc;
 
 use iced::executor;
-use iced::widget::{column, container, horizontal_space, row, text, text_editor};
+use iced::widget::{button, column, container, horizontal_space, row, text, text_editor};
 use iced::{Application, Command, Element, Length, Settings, Theme};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
+#[derive(Debug, Clone)]
+enum Error {
+    DialogClosed,
+    IO(io::ErrorKind),
+}
 struct Editor {
     content: text_editor::Content,
     error: Error,
@@ -20,8 +25,8 @@ struct Editor {
 enum Message {
     Edit(text_editor::Action),
     FileOpened(Result<Arc<String>, Error>),
+    Open,
 }
-
 impl Application for Editor {
     type Message = Message;
     type Theme = Theme;
@@ -34,10 +39,10 @@ impl Application for Editor {
                 content: text_editor::Content::new(),
                 error: Error::DialogClosed,
             },
-            Command::perform(
-                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
-                Message::FileOpened,
-            ),
+            Command::none(), // Command::perform(
+                             //     load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
+                             //     Message::FileOpened,
+                             // ),
         )
     }
 
@@ -49,20 +54,24 @@ impl Application for Editor {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
+                Command::none()
             }
+            Message::Open => Command::perform(pick_file(), Message::FileOpened),
             Message::FileOpened(Ok(file_content)) => {
                 self.content = text_editor::Content::with(&file_content);
+                Command::none()
             }
             Message::FileOpened(Err(error)) => {
                 self.error = error;
+                Command::none()
             }
         }
-
-        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
         let content = {
+            let open_file = button("Open file").on_press(Message::Open);
+            let text_editor = text_editor(&self.content).on_edit(Message::Edit);
             let status_bar = {
                 let cursor_status = {
                     let (line, column) = self.content.cursor_position();
@@ -70,8 +79,7 @@ impl Application for Editor {
                 };
                 row![horizontal_space(Length::Fill), cursor_status]
             };
-            let text_editor = text_editor(&self.content).on_edit(Message::Edit);
-            column![text_editor, status_bar].spacing(5)
+            column![open_file, text_editor, status_bar].spacing(5)
         };
         container(content).padding(10).into()
     }
@@ -97,10 +105,4 @@ async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, Error> {
         .map(Arc::new)
         .map_err(|e| e.kind())
         .map_err(Error::IO)
-}
-
-#[derive(Debug, Clone)]
-enum Error {
-    DialogClosed,
-    IO(io::ErrorKind),
 }
